b218ca387c4379d1f22c76a7193d2210a4e4819c
Yebon Kim <v954242@gmail.com>
2020-12-14 22:29:39 +0900

    [Main] add date text view

--- app/app/src/main/java/com/example/yebon/one_todo/MainActivity.kt
+++ app/app/src/main/java/com/example/yebon/one_todo/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.yebon.one_todo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.time.Year
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private val calendar by lazy {
+        Calendar.getInstance()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        year.text = calendar.get(Calendar.YEAR).toString()
+        month.text = calendar.get(Calendar.MONTH).toString()
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.date_container -> showDatePicker()
+        }
+    }
+
+    private fun showDatePicker() {
+
     }
 }
